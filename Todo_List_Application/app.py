eecdf932c9547da36e5f378a68412870e315ef48
cracka <crackabad859@gmail.com>
2024-03-09 15:22:39 +0300

    finalised todo list applications

--- Todo_List_Application/app.py
+++ Todo_List_Application/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from sqlalchemy import Integer, String
@@ -19,19 +19,41 @@ class Todo(db.Model):
     
 
 
-@app.route("/")
+@app.route("/", methods =['Get', 'POST'])
+
 
 def home():
+    if request.method == 'POST':
+        todo_title = request.form['title']
+        desc_todo = request.form['desc']
+        data = Todo(title = todo_title, desc = desc_todo)
+        db.session.add(data)
+        db.session.commit()
     alltodo = Todo.query.all()
     return render_template('index.html', alltodo = alltodo)
 
-@app.route("/show")
+@app.route("/delete/<int:sno>")
 
-def show_data():
-    alltodo = Todo.query.all()
-    print(alltodo)
-    return "this will rturn data from database"
+def delete(sno):
+   todo = Todo.query.filter_by(sno=sno).first()
+   db.session.delete(todo)
+   db.session.commit()
+   return redirect("/")
 
+@app.route("/update/<int:sno>", methods = ['GET', 'POST'])
+
+def update(sno):
+    if request.method == "POST":
+        todo_title = request.form['title']
+        desc_todo = request.form['desc']
+        data = Todo.query.filter_by(sno = sno).first()
+        data.title = todo_title
+        data.desc = desc_todo
+        db.session.add(data)
+        db.session.commit()
+        return redirect("/")
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html',todo =todo)
 
 if __name__ == "__main__":
     app.run(debug=True)
