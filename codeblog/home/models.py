c15182a9f683f659248b15e0ab8b5d90712b1513
cracka <crackabad859@gmail.com>
2024-03-11 14:47:13 +0300

    model creating and fronted designing

--- codeblog/home/models.py
+++ codeblog/home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    sno = models.AutoField(primary_key= True)
+    name = models.CharField(max_length = 50)
+    phone = models.CharField(max_length = 20)
+    email = models.EmailField(max_length = 50)
+    content = models.TextField()
+
+    
+    def __str__(self) -> str:
+        return self.name
+
+    
--- codeblog/home/views.py
+++ codeblog/home/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Contact
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
     return render(request, 'home/home.html')
 
 def contact(request):
+    messages.success(request, "Welcome to cantact us..happy to hear from you.")
+    if request.method == 'POST':
+        name = request.POST['name']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        content = request.POST['content']
+        if len(name) < 2 or len(email) < 3 or len(phone)< 10 or len(content) < 4:
+            messages.warning(request, "please fill the form correctly")
+        
+        else:
+           contact = Contact(name = name, email = email, phone = phone, content = content)
+           contact.save()
+           messages.success(request, "your form was sent successfully")
+        
     return render(request, 'home/contact.html')
 
 def about(request):
