35471f15113d387b3d54fc1c61272e92fecea2bd
cracka <crackabad859@gmail.com>
2024-03-20 11:31:11 +0300

    user authentication nd authorization initials

--- codeblog/codeblog/settings.py
+++ codeblog/codeblog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'home'
+    'home',
+    'login_signup_and_logout'
 ]
 
 MIDDLEWARE = [
---
+++ codeblog/login_signup_and_logout/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LoginSignupAndLogoutConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'login_signup_and_logout'
